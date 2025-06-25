8c8202a8f7f870625edfcabe67232f544f05541a
Erika Belicova <146355532+Erika-Belicova@users.noreply.github.com>
2025-06-25 16:03:14 +0200

    chore(back): upgrade Java and dependencies
    - Upgrade Java version to 17
    - Update all dependencies to compatible versions
    - Adjust imports where necessary

--- back/src/main/java/com/openclassrooms/mddapi/model/Post.java
+++ back/src/main/java/com/openclassrooms/mddapi/model/Post.java
@@ -1,6 +1,6 @@
 package com.openclassrooms.mddapi.model;
 
-import javax.persistence.*;
+import jakarta.persistence.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
