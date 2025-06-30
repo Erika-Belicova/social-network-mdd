4e3fa219ef4ba27268a9a53ea281894bc5deea8b
Erika Belicova <146355532+Erika-Belicova@users.noreply.github.com>
2025-06-30 23:55:10 +0200

    Add request DTOs for login, register, post, and comment

---
+++ back/src/main/java/com/openclassrooms/mddapi/dto/LoginRequestDTO.java
@@ -0,0 +1,18 @@
+package com.openclassrooms.mddapi.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class LoginRequestDTO {
+
+    private String username;
+
+    @Email
+    private String email;
+
+    @NotBlank
+    private String password;
+
+}
