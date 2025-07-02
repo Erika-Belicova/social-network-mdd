fc6105cb3a6b8da68775d25941e949a96dafd121
Erika Belicova <146355532+Erika-Belicova@users.noreply.github.com>
2025-07-02 16:13:06 +0200

    feat(back): add JWT properties and update security
    - Add JwtProperties class
    - Configure JWT encoder, decoder, and password encoder beans in SecurityConfig
    - Update pom.xml dependencies

--- back/src/main/java/com/openclassrooms/mddapi/response/ErrorResponse.java
+++ back/src/main/java/com/openclassrooms/mddapi/response/ErrorResponse.java
@@ -1,15 +1,14 @@
 package com.openclassrooms.mddapi.response;
 
+import lombok.Data;
+
+@Data
 public class ErrorResponse {
 
     private final String error;
 
     public ErrorResponse(String error) {
         this.error = error;
-    }
-
-    public String getError() {
-        return error;
     }
 
 }
