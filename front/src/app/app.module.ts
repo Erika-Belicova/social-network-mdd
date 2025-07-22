d3575322397b3ef4338764a54ddffa8bfd97fe63
Erika Belicová <146355532+Erika-Belicova@users.noreply.github.com>
2025-07-22 12:51:20 +0200

    feat(front): layout & style for posts, topics & me (#31)
    - Add basic layout and styling for PostList, PostForm, PostDetail, TopicList, Me, and NotFound components using Angular Material
    - Update Login and Register components to match global styling
    - Enable active route color changes in navbar buttons
    - Import MatIconModule in AppModule

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -4,6 +4,10 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
+import { MatOptionModule } from '@angular/material/core';
+import { MatIconModule } from '@angular/material/icon';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
@@ -42,7 +46,11 @@ import { NavbarComponent } from './shared/navbar/navbar.component';
     BrowserAnimationsModule,
     MatButtonModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    FormsModule,
+    MatSelectModule,
+    MatOptionModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent],
