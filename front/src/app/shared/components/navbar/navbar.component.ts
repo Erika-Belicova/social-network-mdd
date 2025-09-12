39ee90c0f50bca65d41ebcd945f90c925d90c471
Erika Belicová <146355532+Erika-Belicova@users.noreply.github.com>
2025-09-12 19:28:46 +0200

    feat(front): add mobile navbar and side menu in NavbarComponent (#44)
    - Add mobile navbar with hamburger toggle button
    - Add side menu for mobile screens with correct placement of buttons
    - Add styling to navbar and adjust behaviour according to the design mockup

--- front/src/app/shared/components/navbar/navbar.component.ts
+++ front/src/app/shared/components/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
+import { AuthService } from 'src/app/features/auth/services/auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -9,7 +10,7 @@ import { Router, NavigationEnd } from '@angular/router';
 export class NavbarComponent {
   currentRoute = '';
 
-  constructor(private router: Router) {
+  constructor(private router: Router, private authService: AuthService) {
     this.router.events.subscribe(event => {
       if (event instanceof NavigationEnd) {
         this.currentRoute = event.urlAfterRedirects;
@@ -26,7 +27,7 @@ export class NavbarComponent {
   }
 
   logout() {
-    // call service to log out user
+    this.authService.logout();
 
     // route to homepage
     this.router.navigate(['/']);
