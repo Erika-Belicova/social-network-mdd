885c5011d329a46e5fadc5508480212d8008873a
Erika Belicová <146355532+Erika-Belicova@users.noreply.github.com>
2025-09-24 22:21:05 +0200

    fix(front): fix layout in MeComponent and clean up unused components (#56)
    - Restrict width of form fields in MeComponent to prevent them from stretching with validation error messages
    - Remove CommentsComponent, as comments logic is handled in PostDetailComponent
    - Remove NavbarComponent, as navbar logic was moved to AppComponent to fix sidenav functionality
    - Remove unused CSS from LoginComponent and RegisterComponent

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -23,9 +23,7 @@ import { SubscriptionsComponent } from './features/user/components/subscriptions
 import { PostListComponent } from './features/posts/components/post-list/post-list.component';
 import { PostDetailComponent } from './features/posts/components/post-detail/post-detail.component';
 import { PostFormComponent } from './features/posts/components/post-form/post-form.component';
-import { CommentsComponent } from './features/posts/components/comments/comments.component';
 import { TopicListComponent } from './features/topics/components/topic-list/topic-list.component';
-import { NavbarComponent } from './shared/components/navbar/navbar.component';
 import { AuthInterceptor } from './core/interceptors/auth.interceptor';
 import { MatSidenavModule } from '@angular/material/sidenav';
 
@@ -46,9 +44,7 @@ registerLocaleData(localeFr);
     PostListComponent,
     PostDetailComponent,
     PostFormComponent,
-    CommentsComponent,
-    TopicListComponent,
-    NavbarComponent
+    TopicListComponent
   ],
   imports: [
     BrowserModule,
